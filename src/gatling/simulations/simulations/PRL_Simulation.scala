5554f82a77ec7b0d069c6a2fe3b825a138326cf0
yogeshruperai <yogesh.ruperai@solirius.com>
2026-01-05 10:49:03 +0000

    Changed max duration back to 90 mins

--- src/gatling/simulations/simulations/PRL_Simulation.scala
+++ src/gatling/simulations/simulations/PRL_Simulation.scala
@@ -749,6 +749,6 @@ class PRL_Simulation extends Simulation {
   setUp(setUpType(testType))
     .protocols(httpProtocol)
     .assertions(assertions(testType))
-    .maxDuration(75 minutes)
+    .maxDuration(90 minutes)
 
 }
